fd1e8233b651a8d54af3ba3fab8f1322c6af4896
WillCui- <wyfcui@gmail.com>
2021-01-22 00:19:12 -0500

    useEffect refactoring, result display

--- frontend/src/Board.js
+++ frontend/src/Board.js
@@ -1,53 +1,46 @@
 import React, { useState, useEffect } from "react";
 import Tile from "./Tile.js";
-import axios from 'axios';
 
 function Board(props) {
-    let data;
-    // axios.get(`http://localhost:5000/letters`).then(res => {
-    //     data = res.data;
-    // });
-    // console.log(data)
-    // // let lets = DataService.getLetters();
-    const [letters, setLetters] = useState([]);
+    // const [letters, setLetters] = useState([]);
 
-    useEffect(() => {
-        function handleKey(e) {
-            const c = e.key;
-            if (e.keyCode === 8) {
-                setLetters(l => l.slice(0, l.length - 1));
-            } else if (letters.length < 16 && c.length === 1 && c.match(/[a-zA-Z]/i)) {
-                setLetters(l => l.concat([c.toUpperCase()]));
+    // useEffect(() => {
+    //     function handleKey(e) {
+    //         const c = e.key;
+    //         if (e.keyCode === 8) {
+    //             setLetters(l => l.slice(0, l.length - 1));
+    //         } else if (letters.length < 16 && c.length === 1 && c.match(/[a-zA-Z]/i)) {
+    //             setLetters(l => l.concat([c.toUpperCase()]));
                 
-            }
-        };
+    //         }
+    //     };
 
-        document.addEventListener('keydown', handleKey);
+    //     document.addEventListener('keydown', handleKey);
 
-        return function cleanup() {
-            document.removeEventListener('keydown', handleKey);
-        }
-    }, [letters]);
+    //     return function cleanup() {
+    //         document.removeEventListener('keydown', handleKey);
+    //     }
+    // }, [letters]);
 
-    if (letters.length === 16) {
-        const formData = new FormData();
-        let messages = '';
-        letters.forEach(l => {
-            messages += l;
-        });
-        formData.append("letters", messages);
-        axios.post(`http://localhost:5000/letters`, formData)
-            .then(res => {
-                console.log("posting", res.data['results']);
-            }).catch(error => {
-                console.log(error.response);
-            });
-    }
+    // if (letters.length === 16) {
+    //     const formData = new FormData();
+    //     let messages = '';
+    //     letters.forEach(l => {
+    //         messages += l;
+    //     });
+    //     formData.append("letters", messages);
+    //     axios.post(`http://localhost:5000/letters`, formData)
+    //         .then(res => {
+    //             // console.log("posting", res.data['results']);
+    //         }).catch(error => {
+    //             console.log(error.response);
+    //         });
+    // }
 
     function renderTile(key) {
         return (
             <Tile 
-                value = {letters[key]}
+                value = {props.letters[key]}
                 key = {key}
                 tabIndex="0"
             />
--- frontend/src/Game.js
+++ frontend/src/Game.js
@@ -1,32 +1,72 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
+
 import Board from './Board.js';
 import Results from './Results.js';
+
 
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Card from "react-bootstrap/Card";
 
-class Game extends React.Component {
-    render() {
-        return (
-            <Card>
-                <Card.Header as="h5">WordHunt Helper</Card.Header>
-                <Card.Body style={{padding: '50px'}}>
-                    <Container>
-                        <Row className="align-items-end">
-                            <Col><Board /></Col>
-                            <Col>
-                                <Results 
-                                    results={['abc', 'def', 'aaaaaaaaaa']}
-                                />
-                            </Col>
-                        </Row>
-                    </Container>
-                </Card.Body>
-            </Card>
-        )
+function Game(props) {
+    const [letters, setLetters] = useState([]);
+    const [results, setResults] = useState([]);
+
+    useEffect(() => {
+        function handleKey(e) {
+            const c = e.key;
+            if (e.keyCode === 8) {
+                setLetters(l => l.slice(0, l.length - 1));
+            } else if (letters.length < 16 && c.length === 1 && c.match(/[a-zA-Z]/i)) {
+                setLetters(l => l.concat([c.toUpperCase()]));
+            }
+        };
+
+        document.addEventListener('keydown', handleKey);
+
+        return function cleanup() {
+            document.removeEventListener('keydown', handleKey);
+        }
+    }, [letters]);
+
+    if (letters.length === 16) {
+        const formData = new FormData();
+        let messages = '';
+        letters.forEach(l => {
+            messages += l;
+        });
+        formData.append("letters", messages);
+        axios.post(`http://localhost:5000/letters`, formData)
+            .then(res => {
+                setResults(res.data['results']);
+            }).catch(error => {
+                console.log(error.response);
+            });
     }
+
+    return (
+        <Card>
+            <Card.Header as="h5">WordHunt Helper</Card.Header>
+            <Card.Body style={{padding: '50px'}}>
+                <Container>
+                    <Row className="align-items-end">
+                        <Col>
+                            <Board 
+                                letters={ letters }
+                            />
+                        </Col>
+                        <Col>
+                            <Results 
+                                results={ results }
+                            />
+                        </Col>
+                    </Row>
+                </Container>
+            </Card.Body>
+        </Card>
+    );
 }
 
 export default Game;
